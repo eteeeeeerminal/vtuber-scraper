0787e574edcaadb57d8131e00b695c47c6ba9057
えてみなる <47494433+eteeeeeerminal@users.noreply.github.com>
2022-11-25 11:52:41 +0900

    feat: logger でログファイルを出力するように変更

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -8,4 +8,8 @@ def get_logger(modname: str, loglevel=logging.DEBUG) -> logging.Logger:
     logger.addHandler(handler)
     logger.propagate = False
 
+    file_handler = logging.FileHandler(filename="vtuber-scraper.log", encoding="utf-8")
+    file_handler.setLevel(loglevel)
+    logger.addHandler(file_handler)
+
     return logger
